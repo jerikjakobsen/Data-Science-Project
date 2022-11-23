503d42ac4bacbad2417b5e940abd804f36a702e1
John Jakobsen <jerikjakobsen@gmail.com>
2022-11-22 19:22:27 -0500

    Updated Correlations csv to include days on market

--- Data Analysis/correlations.py
+++ Data Analysis/correlations.py
@@ -4,5 +4,5 @@ import os
 filePath = "../CSVFiles/FilteredListing+SaleData.csv"
 data = pd.read_csv(filePath, sep=",")
 data = data[data['sale_price'] > 0]
-data = data.drop(['Address', "State", "City", "crawl_date", "listing_date"], axis = 1)
-data.corr()[['Days On Trulia', 'sale_price']].to_csv("correlations.csv")
+data = data.drop(['Address', "State", "City", "listing_date"], axis = 1)
+data.corr()[['Days on Market', 'sale_price']].to_csv("correlations.csv")
