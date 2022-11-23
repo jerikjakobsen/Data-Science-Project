f60b21f7855b883f325b0354137c3d9f73965595
John Jakobsen <jerikjakobsen@gmail.com>
2022-11-22 19:19:32 -0500

    Added Days on Market column

--- Data Cleaning/MergeSalesAndTrulia.py
+++ Data Cleaning/MergeSalesAndTrulia.py
@@ -8,7 +8,7 @@ def mergeSaleAndTruliaData(sales, trulia):
     dfJoined = sales.merge(trulia, on=["Address", "City", "State"])
     dfFiltered = dfJoined[dfJoined["listing_date"] < dfJoined["sale_date"]]
     dfFiltered = dfFiltered.sort_values('sale_date').drop_duplicates(["Address", "City", "State"], keep='first')
-    dfFiltered["Days on Market"] = pd.to_datetime(dfFiltered['sale_date']).dt.date- pd.to_datetime(dfFiltered['listing_date']).dt.date
+    dfFiltered["Days on Market"] = (pd.to_datetime(dfFiltered['sale_date']).dt.date- pd.to_datetime(dfFiltered['listing_date']).dt.date).dt.days
     return dfFiltered
 dfFiltered = mergeSaleAndTruliaData(filteredSalesDF, filteredTruliaDF)
 dfFiltered.to_csv('../CSVFiles/FilteredListing+SaleData.csv', index = False)
